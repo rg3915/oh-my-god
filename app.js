fb55d5e6748cebca7c072b1f1895c0c69da92979
Guilherme <guilhermee.paixaoo@gmail.com>
2015-03-02 20:14:51 -0300

    Add on Heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('./config/express')
-,	db  =  require('./config/db')('mongodb://127.0.0.1/omg')
+,	db  =  require('./config/db')('mongodb://guilherme:Guiferpa94@ds049651.mongolab.com:49651/omg')
 , 	http = require('http').Server(app);
 
-app.listen(app.get('port'), function(){
+app.listen(app.get('port') || process.env.PORT, function(){
 	console.log('Servidor certo');
 });
